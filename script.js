628794928abb9d99748277420c410194dd1cac22
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-03 16:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const submitButton = document.querySelector("#submit");
+const bookList = document.getElementById("book-list");
+
+submitButton.addEventListener("click",addBook);
+
+function addBook(){
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const isbn = document.getElementById("isbn").value;
+
+    // create new row
+    const newRow=document.createElement("tr");
+    newRow.innerHTML=`
+    <td>${title}</td>
+    <td>${author}</td>
+    <td>${isbn}</td>
+    <td><button class="delete">Clear</button></td>
+    `;
+
+    //Add the row
+    bookList.append(newRow);
+
+    //Delete
+    const deleteButton=newRow.querySelector(".delete");
+    deleteButton.addEventListener("click", ()=>{
+        newRow.remove();
+    });
+    
+    //clear input fields
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+}
